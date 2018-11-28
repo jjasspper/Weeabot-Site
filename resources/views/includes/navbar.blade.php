342c953a2877ee23ed2c3ca603416f74624228b8
Jasper van Hienen <jasperh01@hotmail.com>
2018-11-28 13:40:36 +0100

    Update navbar.blade.php

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -5,7 +5,7 @@
         <ul class="navbar__menu__list">
            <li class="navbar__menu__item"><a href="#about" class="navbar__menu__link">About</a></li>
            <li class="navbar__menu__item"><a href="https://github.com/jjasspper/Weeaboo-Detector"  target="_blank" class="navbar__menu__link">GitHub</a></li>
-           <li class="navbar__menu__item is-special"><a href="https://discordapp.com/oauth2/authorize?client_id=423130765090357248&scope=bot&permissions=939617302" target="_blank" class="navbar__menu__link">Invite now!</a></li>
+           <li class="navbar__menu__item is-special"><a href="https://discordapp.com/oauth2/authorize?client_id=423130765090357248&scope=bot&permissions=268446726" target="_blank" class="navbar__menu__link">Invite now!</a></li>
         </ul>
     </div>
 </nav>
